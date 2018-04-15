21fb72e13949e88599d4c5d05881f0283766eab5
Marina Gallardo <marinagallardo@gmail.com>
2018-04-15 13:08:01 +0200

    responsive mejoras

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
     gulp.src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
         .pipe(autoprefixer())
         .pipe(gulp.dest('dist/css/'));
 });
