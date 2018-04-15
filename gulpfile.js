d7e96f88f122173dc546618e0f2503badff91de0
Marina Gallardo <marinagallardo@gmail.com>
2018-04-15 13:18:11 +0200

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
     gulp.src('src/scss/**/*.scss')
-        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write())
         .pipe(autoprefixer())
         .pipe(gulp.dest('dist/css/'));
 });
